ddbdea5b450a9f5f3f5a31c4a690719f2710c460
Toni Rosinol <tonirosinol@hotmail.com>
2018-01-09 12:24:12 -0500

    Add script to execute example code

---
+++ scripts/stereoVIOEuroc.bash
@@ -0,0 +1,13 @@
+ PROJECT_PATH="/home/tonirv/code/spark_vio"
+ DATASET_PATH="/home/tonirv/datasets/EuRoC/V1_01_easy"
+
+# Execute stereoVIOEuroc with given flags. The flag --help will provide you with information about what each flag
+# does.
+$PROJECT_PATH/build/stereoVIOEuroc \
+--dataset_path="$DATASET_PATH" \
+--vio_params_path= "$PROJECT_PATH/params/vioParameters.yaml" \
+--tracker_params_path= "$PROJECT_PATH/params/trackerParameters.yaml" \
+--viz_type=2 \
+--log_output=false \
+--v=0 # Verbosity level.
+
