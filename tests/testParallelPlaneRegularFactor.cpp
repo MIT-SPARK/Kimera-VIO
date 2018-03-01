42af7eacb40163655147db5c798c693674bbaa12
Toni Rosinol <tonirosinol@hotmail.com>
2018-03-01 14:33:49 -0500

    Add testCoplanarPlaneBasicRegularFactor

--- tests/testParallelPlaneRegularFactor.cpp
+++ tests/testParallelPlaneRegularFactor.cpp
@@ -251,6 +251,49 @@ TEST(testCoplanarPlaneTangentSpaceRegularFactor, PlaneOptimization) {
   CHECK(assert_equal(expected, result, tol))
 }
 
+TEST(testCoplanarPlaneBasicRegularFactor, PlaneOptimization) {
+  /// Two planes constrained together, using co-planarity basic factor.
+  /// With one of the planes having a prior.
+  NonlinearFactorGraph graph;
+  Key planeKey1 = 7;
+  Key planeKey2 = 8;
+
+  /// Shared noise for all landmarks.
+  noiseModel::Diagonal::shared_ptr priorNoise =
+          noiseModel::Diagonal::Sigmas(Vector3(0.1, 0.1, 0.1));
+
+  /// Prior mean for plane 1.
+  OrientedPlane3 priorMean1(0.0, 0.0, 1.0, 0.0);
+  graph.emplace_shared<PriorFactor<OrientedPlane3> >(planeKey1, priorMean1, priorNoise);
+
+  /// Shared noise for constraints between planes.
+  noiseModel::Diagonal::shared_ptr regularityNoise =
+          noiseModel::Diagonal::Sigmas(Vector4(0.1, 0.1, 0.1, 0.1));
+
+  /// Factor between planes for co-planarity.
+  graph.emplace_shared<CoplanarPlaneRegularBasicFactor>(
+              planeKey1, planeKey2, regularityNoise);
+
+  Values initial;
+  initial.insert(planeKey1, OrientedPlane3(0.1, 0.2, 0.9, 0.0));
+  initial.insert(planeKey2, OrientedPlane3(0.1, 0.2, 0.3, 2.0));
+
+  GaussNewtonParams params;
+  params.setVerbosity("ERROR");
+  params.setMaxIterations(20);
+  params.setRelativeErrorTol(-std::numeric_limits<double>::max());
+  //params.setErrorTol(-std::numeric_limits<double>::max());
+  params.setAbsoluteErrorTol(-std::numeric_limits<double>::max());
+
+  Values result = GaussNewtonOptimizer(graph, initial, params).optimize();
+  //Values result = LevenbergMarquardtOptimizer(graph, initial, params).optimize();
+
+  Values expected;
+  expected.insert(planeKey1, OrientedPlane3(0.0, 0.0, 1.0, 0.0));
+  expected.insert(planeKey2, OrientedPlane3(0.0, 0.0, 1.0, 0.0));
+
+  CHECK(assert_equal(expected, result, tol))
+}
 /* ************************************************************************* */
 int main() {
   TestResult tr; return TestRegistry::runAllTests(tr); }
