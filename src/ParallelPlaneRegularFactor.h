5fda5996ee6fa8d4bb70bc1eae627e62d075f205
Toni Rosinol <tonirosinol@hotmail.com>
2018-03-01 14:37:33 -0500

    Remove Coplanar and use only GeneralParallelPlaneRegularBasicFactor

--- src/ParallelPlaneRegularFactor.h
+++ src/ParallelPlaneRegularFactor.h
@@ -201,54 +201,6 @@ private:
   }
 };
 
-class CoplanarPlaneRegularBasicFactor: public ParallelPlaneRegularFactor {
-public:
-  /// Constructor
-  CoplanarPlaneRegularBasicFactor() {
-  }
-  virtual ~CoplanarPlaneRegularBasicFactor() {}
-
-  CoplanarPlaneRegularBasicFactor(const Key& plane1Key,
-                                         const Key& plane2Key,
-                                         const SharedGaussian& noiseModel) :
-                ParallelPlaneRegularFactor(plane1Key, plane2Key, noiseModel) {
-      this->factor_type_ = "Coplanar Basic Factor";
-  }
-
-private:
-  /// evaluateError
-  /// Hplane1: jacobian of h wrt plane1
-  /// Hplane2: jacobian of h wrt plane2
-  virtual Vector doEvaluateError(
-                       const OrientedPlane3& plane_1,
-                       const OrientedPlane3& plane_2,
-                       boost::optional<Matrix&> H_plane_1,
-                       boost::optional<Matrix&> H_plane_2) const {
-    Unit3 plane_normal_1 = plane_1.normal();
-    Unit3 plane_normal_2 = plane_2.normal();
-    Vector4 err (0, 0, 0, 0);
-    err =  Vector4(plane_normal_1.point3().x() - plane_normal_2.point3().x(),
-                   plane_normal_1.point3().y() - plane_normal_2.point3().y(),
-                   plane_normal_1.point3().z() - plane_normal_2.point3().z(),
-                   plane_1.distance() - plane_2.distance());
-    if (H_plane_1) {
-      // Jacobian of plane retraction when v = Vector3::Zero(), to speed-up
-      // computations.
-      Matrix43 tmp;
-      tmp << plane_normal_1.basis(), Vector3::Zero(), 0, 0, 1;
-      *H_plane_1 = tmp;
-    }
-    if (H_plane_2) {
-      // Jacobian of plane retraction when v = Vector3::Zero(), to speed-up
-      // computations.
-      Matrix43 tmp;
-      tmp << -plane_normal_2.basis(), Vector3::Zero(), 0, 0, -1;
-      *H_plane_2 = tmp;
-    }
-    return (err);
-  }
-};
-
 class GeneralParallelPlaneRegularBasicFactor: public ParallelPlaneRegularFactor {
 public:
   /// Constructor
@@ -257,9 +209,9 @@ public:
   virtual ~GeneralParallelPlaneRegularBasicFactor() {}
 
   GeneralParallelPlaneRegularBasicFactor(const Key& plane1Key,
-                        const Key& plane2Key,
-                        const SharedGaussian& noiseModel,
-                        const double& measured_distance_from_plane2_to_plane1) :
+                    const Key& plane2Key,
+                    const SharedGaussian& noiseModel,
+                    const double& measured_distance_from_plane2_to_plane1 = 0) :
           ParallelPlaneRegularFactor(plane1Key, plane2Key, noiseModel,
                                      measured_distance_from_plane2_to_plane1) {
       this->factor_type_ = "Parallelism + Distance Basic Factor between Planes";
