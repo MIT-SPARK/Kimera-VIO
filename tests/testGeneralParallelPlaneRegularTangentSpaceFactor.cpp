3c2a6ea9615eeca967ef1c1a023f47519a1dc4df
Toni Rosinol <tonirosinol@hotmail.com>
2018-03-28 17:16:46 -0400

    Add tests for GeneralParallelPlaneRegularTangentSpaceFactor

--- tests/testGeneralParallelPlaneRegularTangentSpaceFactor.cpp
+++ tests/testGeneralParallelPlaneRegularTangentSpaceFactor.cpp
@@ -38,82 +38,135 @@ using namespace gtsam;
 static const double tol = 1e-5;
 static const double der_tol = 1e-5;
 
+/**
+  * Test that error does give the right result when it is zero.
 /* ************************************************************************* */
-//TEST(testParallelPlaneRegularFactor, Jacobians) {
-//  // Create the factor with a measurement that is 3 pixels off in x
-//  Key pointKey(1);
-//  Key planeKey(2);
-//  noiseModel::Diagonal::shared_ptr regularityNoise =
-//          noiseModel::Diagonal::Sigmas(Vector1(0.1));
-//  BasicRegularPlane3Factor factor(pointKey, planeKey, regularityNoise);
-//
-//  double plane_normal [3] = {2.4, 1.2, 1.9};
-//  double distance(2.3);
-//
-//  // Set the linearization point
-//  Point3 point(4.3, 3.2, 1.9);
-//  OrientedPlane3 plane(Unit3(plane_normal[0], plane_normal[1], plane_normal[2]),
-//                       distance);
-//
-//  // Use the factor to calculate the Jacobians
-//  gtsam::Matrix H1Actual, H2Actual;
-//  factor.evaluateError(point, plane, H1Actual, H2Actual);
-//
-//  // Calculate numerical derivatives
-//  Matrix H1Expected = numericalDerivative21<Vector, Point3, OrientedPlane3>(
-//      boost::bind(&BasicRegularPlane3Factor::evaluateError, &factor, _1, _2,
-//          boost::none, boost::none), point, plane, der_tol);
-//
-//  Matrix H2Expected = numericalDerivative22<Vector, Point3, OrientedPlane3>(
-//      boost::bind(&BasicRegularPlane3Factor::evaluateError, &factor, _1, _2,
-//          boost::none, boost::none), point, plane, der_tol);
-//
-//  // Verify the Jacobians are correct
-//  CHECK(assert_equal(H1Expected, H1Actual, tol));
-//  CHECK(assert_equal(H2Expected, H2Actual, tol));
-//}
-//
-//TEST(testBasicRegularPlane3Factor, JacobiansNegative) {
-//  // Create the factor with a measurement that is 3 pixels off in x
-//  Key pointKey(1);
-//  Key planeKey(2);
-//  noiseModel::Diagonal::shared_ptr regularityNoise =
-//          noiseModel::Diagonal::Sigmas(Vector1(0.1));
-//  BasicRegularPlane3Factor factor(pointKey, planeKey, regularityNoise);
-//
-//  double plane_normal [3] = {2.4, 1.2, 1.9};
-//  double distance(-2.3);
-//
-//  // Set the linearization point
-//  Point3 point(4.3, 3.2, 1.9);
-//  OrientedPlane3 plane(Unit3(plane_normal[0], plane_normal[1], plane_normal[2]),
-//                       distance);
-//
-//  // Use the factor to calculate the Jacobians
-//  gtsam::Matrix H1Actual, H2Actual;
-//  factor.evaluateError(point, plane, H1Actual, H2Actual);
-//
-//  // Calculate numerical derivatives
-//  Matrix H1Expected = numericalDerivative21<Vector, Point3, OrientedPlane3>(
-//      boost::bind(&BasicRegularPlane3Factor::evaluateError, &factor, _1, _2,
-//          boost::none, boost::none), point, plane, der_tol);
-//
-//  Matrix H2Expected = numericalDerivative22<Vector, Point3, OrientedPlane3>(
-//      boost::bind(&BasicRegularPlane3Factor::evaluateError, &factor, _1, _2,
-//          boost::none, boost::none), point, plane, der_tol);
-//
-//  // Verify the Jacobians are correct
-//  CHECK(assert_equal(H1Expected, H1Actual, tol));
-//  CHECK(assert_equal(H2Expected, H2Actual, tol));
-//}
+TEST(testGeneralParallelPlaneRegularTangentSpaceFactor, ErrorIsZero) {
+  /// Plane keys.
+  Key plane_key_1 (1);
+  Key plane_key_2 (2);
 
+  /// Noise model for cosntraint between the two planes.
+  noiseModel::Diagonal::shared_ptr parallel_plane_noise =
+      noiseModel::Diagonal::Sigmas(Vector3(0.1, 0.1, 0.1));
+
+
+  /// Measured distance between plane.
+  double measured_distance_from_plane2_to_plane1 = 0.8;
+
+  /// GeneralParallelism constraint between Plane 1 and Plane 2.
+  GeneralParallelPlaneRegularTangentSpaceFactor factor(plane_key_1, plane_key_2,
+                                       parallel_plane_noise,
+                                       measured_distance_from_plane2_to_plane1);
+
+  /// Planes.
+  OrientedPlane3 plane_1(0.1, 0.1, 0.9, 0.9);
+  OrientedPlane3 plane_2(0.1, 0.1, 0.9, 0.1);
+
+  /// Calculate error.
+  Vector error = factor.evaluateError(plane_1, plane_2);
+
+  /// Expected error.
+  Vector3 expected_error = Vector3::Constant(0.0);
+
+  EXPECT(assert_equal(expected_error, error, tol))
+}
+
+/**
+  * Test that error does give the right result when it is not zero.
+/* ************************************************************************* */
+TEST(testGeneralParallelPlaneRegularTangentSpaceFactor, ErrorOtherThanZero) {
+  /// Plane keys.
+  Key plane_key_1 (1);
+  Key plane_key_2 (2);
+
+  /// Noise model for cosntraint between the two planes.
+  noiseModel::Diagonal::shared_ptr parallel_plane_noise =
+      noiseModel::Diagonal::Sigmas(Vector3(0.1, 0.1, 0.1));
+
+  /// Measured distance between plane.
+  double measured_distance_from_plane2_to_plane1 = 0.7;
+
+  /// GeneralParallelism constraint between Plane 1 and Plane 2.
+  GeneralParallelPlaneRegularTangentSpaceFactor factor(plane_key_1, plane_key_2,
+                                       parallel_plane_noise,
+                                       measured_distance_from_plane2_to_plane1);
+
+  /// Planes.
+  OrientedPlane3 plane_1(0.0, 0.0, 1.0, 0.9);
+  OrientedPlane3 plane_2(0.1, 0.1, 0.9, 0.1);
+
+  /// Calculate error.
+  Vector error = factor.evaluateError(plane_1, plane_2);
+
+  /// Expected error.
+  Vector3 expected_error;
+  expected_error << 0.109764, -0.109764, 0.1;
+
+  EXPECT(assert_equal(expected_error, error, tol))
+}
+
+/**
+  * Test that analytical jacobians equal numerical ones.
+  *
+/* ************************************************************************* */
+TEST(testGeneralParallelPlaneRegularFactor, Jacobians) {
+  /// Plane keys.
+  Key plane_key_1 (1);
+  Key plane_key_2 (2);
+
+  /// Noise model for cosntraint between the two planes.
+  noiseModel::Diagonal::shared_ptr parallel_plane_noise =
+      noiseModel::Diagonal::Sigmas(Vector3(0.1, 0.1, 0.1));
+
+  /// GeneralParallelism constraint between Plane 1 and Plane 2.
+  GeneralParallelPlaneRegularTangentSpaceFactor factor(plane_key_1, plane_key_2,
+                                                       parallel_plane_noise);
+
+  /// Planes.
+  OrientedPlane3 plane_1(0.3, 0.2, 1.9, 0.9);
+  OrientedPlane3 plane_2(0.1, 0.1, 0.9, 0.1);
+
+  /// Use the factor to calculate the Jacobians.
+  gtsam::Matrix H1Actual, H2Actual;
+  factor.evaluateError(plane_1, plane_2, H1Actual, H2Actual);
+
+  /// Calculate numerical derivatives.
+  Matrix H1Expected = numericalDerivative21<Vector,
+                                            OrientedPlane3, OrientedPlane3>(
+      boost::bind(&GeneralParallelPlaneRegularTangentSpaceFactor::evaluateError, &factor, _1, _2,
+          boost::none, boost::none), plane_1, plane_2, der_tol);
+
+  Matrix H2Expected = numericalDerivative22<Vector,
+                                            OrientedPlane3, OrientedPlane3>(
+      boost::bind(&GeneralParallelPlaneRegularTangentSpaceFactor::evaluateError, &factor, _1, _2,
+          boost::none, boost::none), plane_1, plane_2, der_tol);
+
+  /// Verify the Jacobians are correct.
+  CHECK(assert_equal(H1Expected, H1Actual, tol));
+  CHECK(assert_equal(H2Expected, H2Actual, tol));
+}
+
+/**
+  * Test that optimization works.
+  * Two planes constrained together, using distance + parallelism factor,
+  *  in tangent space. With one of the planes having a prior.
+  *
+  *                  Prior
+  *                   +-+        Parallelism +
+  *                   +-+        Distance
+  *                    |           factor
+  *                +---+---+        +-+         +-------+
+  *                |Plane 1+--------+ +---------+Plane 2|
+  *                +-------+        +-+         +-------+
+  *
+/* ************************************************************************* */
 /* ************************************************************************* */
 TEST(testGeneralParallelPlaneRegularTangentSpaceFactor, PlaneOptimization) {
-  /// Two planes constrained together, using distance + parallelism factor,
-  ///  in tangent space. With one of the planes having a prior.
   NonlinearFactorGraph graph;
-  Key planeKey1 = 7;
-  Key planeKey2 = 8;
+
+  Key planeKey1 = 1;
+  Key planeKey2 = 2;
 
   /// Shared noise for all landmarks.
   noiseModel::Diagonal::shared_ptr priorNoise =
@@ -135,7 +188,7 @@ TEST(testGeneralParallelPlaneRegularTangentSpaceFactor, PlaneOptimization) {
 
   Values initial;
   initial.insert(planeKey1, OrientedPlane3(0.1, 0.2, 0.9, 0.0));
-  initial.insert(planeKey2, OrientedPlane3(0.1, 0.2, 0.3, -2.0));
+  initial.insert(planeKey2, OrientedPlane3(0.1, 0.2, 0.3, -1.0));
 
   GaussNewtonParams params;
   //params.setVerbosity("ERROR");
