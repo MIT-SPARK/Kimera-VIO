7ec9cd09fe6dda79e76759e9900f625f3260b4b9
Toni Rosinol <tonirosinol@hotmail.com>
2018-04-12 15:35:08 -0400

    Color the output

--- scripts/stereoVIOEuroc.bash
+++ scripts/stereoVIOEuroc.bash
@@ -4,6 +4,8 @@
 # Execute stereoVIOEuroc with given flags. The flag --help will provide you with information about what each flag
 # does.
 $PROJECT_PATH/build/stereoVIOEuroc \
+--logtostderr=1 \
+--colorlogtostderr=1 \
 --dataset_path="$DATASET_PATH" \
 --vio_params_path= "$PROJECT_PATH/params/vioParameters.yaml" \
 --tracker_params_path= "$PROJECT_PATH/params/trackerParameters.yaml" \
