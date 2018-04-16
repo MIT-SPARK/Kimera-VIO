8def62a2978f292fc39438c96c0efbb91aeaa356
Toni Rosinol <tonirosinol@hotmail.com>
2018-04-16 15:57:00 -0400

    Refactor testRegularVIOBackEnd

--- tests/testRegularVioBackEnd.cpp
+++ tests/testRegularVioBackEnd.cpp
@@ -29,26 +29,27 @@ using namespace std;
 using namespace VIO;
 using namespace cv;
 
-static const double tol = 1e-7;
+static constexpr double tol = 1e-7;
 
 /* ************************************************************************* */
 // Parameters
-static const int num_key_frames = 10; // number of frames of the synthetic scene
+static constexpr int num_key_frames = 10; // number of frames of the synthetic scene
 static const Vector3 p0(0, 0, 0); // initial pose of the robot camera
 static const Vector3 v(1.0, 0, 0); // velocity of the robot, per time_step
-static const int time_step = 1e9; // elapsed time between two consecutive frames is 1 second (1e9 nsecs)
-static const Timestamp t_start = 1e9; // ImuBuffer does not allow t = 0;
-static const double baseline = 0.5;
+static constexpr int time_step = 1e9; // elapsed time between two consecutive frames is 1 second (1e9 nsecs)
+static constexpr Timestamp t_start = 1e9; // ImuBuffer does not allow t = 0;
+static constexpr double baseline = 0.5;
 static const imuBias::ConstantBias imu_bias(Vector3(0.1, -0.1, 0.3),
     Vector3(0.1, 0.3, -0.2));
 
 using StereoPoses = vector<pair<Pose3, Pose3>>;
 
-/* ************************************************************************* */
+/* ************************************************************************** */
 // Helper functions!
 vector<Point3> CreateScene() {
   vector<Point3> points;
-  // a scene with 8 points
+
+  // A scene with 8 points.
   points.push_back(Point3(0, 0, 20));
   points.push_back(Point3(0, 20, 20));
   points.push_back(Point3(20, 20, 20));
@@ -61,21 +62,27 @@ vector<Point3> CreateScene() {
 
   return points;
 }
-/* ------------------------------------------------------------------------- */
-StereoPoses CreateCameraPoses(const int num_keyframes,
-    const double baseline, const Vector3 p0, const Vector3 v) {
+/* -------------------------------------------------------------------------- */
+StereoPoses CreateCameraPoses(
+    const int& num_keyframes,
+    const double& baseline,
+    const Vector3& p0,
+    const Vector3& v) {
 
   StereoPoses poses;
   poses.reserve(num_keyframes);
   Pose3 L_pose_R(Rot3::identity(),
-      Vector3(baseline, 0, 0));
+                 Vector3(baseline, 0, 0));
 
-  // The camera is assumed to face (0, 0, 1): z-forward, y down and x to the right
+  // The camera is assumed to face (0, 0, 1):
+  // z-forward, y down and x to the right
   for (int f_id = 0; f_id < num_keyframes; f_id++) {
-    // constant velocity model along x: the robot moves to the right, keeping the point in front of the camera
+    // Constant velocity model along x:
+    // the robot moves to the right, keeping the point in front of the camera
     Vector3 p_offset = v * f_id * (time_step / ((double) 1e9));
 
-    Pose3 pose_left(Rot3::identity(), p0 + p_offset);
+    Pose3 pose_left(Rot3::identity(),
+                    p0 + p_offset);
     Pose3 pose_right = pose_left.compose(L_pose_R);
 
     poses.push_back(make_pair(pose_left, pose_right));
@@ -83,45 +90,47 @@ StereoPoses CreateCameraPoses(const int num_keyframes,
   return poses;
 }
 
-/* ------------------------------------------------------------------------- */
-void CreateImuBuffer(ImuFrontEnd& imu_buf, const int num_frames, const Vector3 v,
-    const ImuBias imu_bias, const Vector3 n_gravity,
-    const Timestamp time_step, const Timestamp t_start) {
-  // Synthesize IMU measurements
+/* -------------------------------------------------------------------------- */
+void CreateImuBuffer(ImuFrontEnd& imu_buf,
+                     const int num_frames,
+                     const Vector3 v,
+                     const ImuBias imu_bias, const Vector3 n_gravity,
+                     const Timestamp time_step,
+                     const Timestamp t_start) {
+  // Synthesize IMU measurements.
   for (int f_id = 0; f_id < num_frames; f_id++) {
-
     Vector6 acc_gyr;
-    // constant speed, no acceleration
+    // Constant speed, no acceleration.
     acc_gyr.head(3) = -n_gravity + imu_bias.accelerometer();
-    // Camera axis aligned with the world axis in this example
+    // Camera axis aligned with the world axis in this example.
     acc_gyr.tail(3) = imu_bias.gyroscope();
     Timestamp t = ((int64_t) f_id) * time_step + t_start;
     imu_buf.insert(t, acc_gyr);
   }
 }
 
-/* ************************************************************************* */
+/* ************************************************************************** */
 TEST(testRegularVio, robotMovingWithConstantVelocity) {
-  // Additional parameters
+  // Additional parameters.
   VioBackEndParams vioParams;
-  vioParams.landmarkDistanceThreshold_ = 30; // we simulate points 20m away
+  vioParams.landmarkDistanceThreshold_ = 30; // We simulate points 20m away.
   vioParams.imuIntegrationSigma_ = 1e-4;
   vioParams.horizon_ = 100;
 
   // Create 3D points
-  vector<Point3> pts = CreateScene();
-  const int num_pts = pts.size();
+  vector<Point3> lmk_pts = CreateScene();
+  const int num_lmk_pts = lmk_pts.size();
 
-  // Create cameras
-  double fov = M_PI / 3 * 2;
-  // Create image size to initiate meaningful intrinsic camera matrix
-  double img_height = 600;
-  double img_width = 800;
-  double fx = img_width / 2 / tan(fov / 2);
-  double fy = fx;
-  double s = 0;
-  double u0 = img_width / 2;
-  double v0 = img_height / 2;
+  // Create cameras.
+  static const double fov = M_PI / 3 * 2;
+  // Create image size to initiate meaningful intrinsic camera matrix.
+  static constexpr double img_height = 600;
+  static constexpr double img_width = 800;
+  static const double fx = img_width / 2 / tan(fov / 2);
+  static const double fy = fx;
+  static constexpr double s = 0;
+  static const double u0 = img_width / 2;
+  static const double v0 = img_height / 2;
 
   Cal3_S2 cam_params(fx, fy, s, u0, v0);
 
@@ -130,65 +139,80 @@ TEST(testRegularVio, robotMovingWithConstantVelocity) {
   ImuFrontEnd imu_buf;
   poses = CreateCameraPoses(num_key_frames, baseline, p0, v);
   CreateImuBuffer(imu_buf, num_key_frames, v, imu_bias,
-      vioParams.n_gravity_, time_step, t_start);
+                  vioParams.n_gravity_,
+                  time_step, t_start);
 
   // Create measurements
   //    using SmartStereoMeasurement = pair<LandmarkId,StereoPoint2>;
   //    using SmartStereoMeasurements = vector<SmartStereoMeasurement>;
-  //    using StatusSmartStereoMeasurements = pair<TrackerStatusSummary,SmartStereoMeasurements>;
+  //    using StatusSmartStereoMeasurements =
+  //                        pair<TrackerStatusSummary, SmartStereoMeasurements>;
   TrackerStatusSummary tracker_status_valid;
   tracker_status_valid.kfTrackingStatus_mono_ = Tracker::VALID;
   tracker_status_valid.kfTrackingStatus_stereo_ = Tracker::VALID;
 
   vector<StatusSmartStereoMeasurements> all_measurements;
   for (int i = 0; i < num_key_frames; i++) {
-    PinholeCamera<Cal3_S2> cam_left(poses[i].first, cam_params);
-    PinholeCamera<Cal3_S2> cam_right(poses[i].second, cam_params);
+    PinholeCamera<Cal3_S2> cam_left(poses.at(i).first,
+                                    cam_params);
+    PinholeCamera<Cal3_S2> cam_right(poses.at(i).second,
+                                     cam_params);
     SmartStereoMeasurements measurement_frame;
-    for (int l_id = 0; l_id < num_pts; l_id++) {
-      Point2 pt_left = cam_left.project(pts[l_id]);
-      Point2 pt_right = cam_right.project(pts[l_id]);
-      StereoPoint2 pt_lr(pt_left.x(), pt_right.x(), pt_left.y());
-      EXPECT_DOUBLES_EQUAL(pt_left.y(), pt_right.y(), 1e-7);
-      measurement_frame.push_back(make_pair(l_id, pt_lr));
+    for (int lmk_id = 0; lmk_id < num_lmk_pts; lmk_id++) {
+      Point2 px_left  = cam_left.project (lmk_pts.at(lmk_id));
+      Point2 px_right = cam_right.project(lmk_pts.at(lmk_id));
+      StereoPoint2 px_lr(px_left.x(),
+                         px_right.x(),
+                         px_left.y());
+      EXPECT_DOUBLES_EQUAL(px_left.y(),
+                           px_right.y(),
+                           tol);
+      measurement_frame.push_back(make_pair(lmk_id, px_lr));
     }
-    all_measurements.push_back(make_pair(tracker_status_valid,measurement_frame));
+    all_measurements.push_back(make_pair(tracker_status_valid,
+                                         measurement_frame));
   }
 
-  // create vio
-  Pose3 B_pose_camLrect(Rot3::identity(), Vector3::Zero());
-  boost::shared_ptr<RegularVioBackEnd> vio =
+  // Create regular VIO.
+  Pose3 B_pose_camLrect(Rot3::identity(),
+                        Vector3::Zero());
+  boost::shared_ptr<RegularVioBackEnd> regular_vio =
       boost::make_shared<RegularVioBackEnd>(B_pose_camLrect,
-                                            cam_params, baseline, vioParams);
+                                            cam_params,
+                                            baseline,
+                                            vioParams);
 
-  // initializeStateAndSetPriors
-  vio->initializeStateAndSetPriors(t_start, poses[0].first, v, imu_bias);
+  regular_vio->initializeStateAndSetPriors(t_start,
+                                           poses.at(0).first,
+                                           v,
+                                           imu_bias);
 
-  // For each frame, add landmarks and optimize
-  for(int64_t k = 1; k < num_key_frames; k++) {
-    // Time stamp for the current keyframe and the next frame
+  // TODO add regularities vector.
+  // TODO fill mesh_lmk_ids with a triplet of lmk_ids.
+  LandmarkIds mesh_lmk_ids_ground_cluster;
+
+  // For each frame, add landmarks and optimize.
+  for (int64_t k = 1; k < num_key_frames; k++) {
+    // Timestamp for the current keyframe and the next frame.
     Timestamp timestamp_lkf = (k - 1) * time_step + t_start;
     Timestamp timestamp_k = k * time_step + t_start;
 
-    // Get the IMU data
+    // Get the IMU data.
     ImuStamps imu_stamps;
     ImuAccGyr imu_accgyr;
     tie(imu_stamps, imu_accgyr) = imu_buf.getBetweenValuesInterpolated(
-        timestamp_lkf, timestamp_k);
+                                                    timestamp_lkf, timestamp_k);
 
-    // process data with VIO
-    // TODO add regularities vector.
-    // TODO fill mesh_lmk_ids with a triplet of lmk_ids.
-    LandmarkIds mesh_lmk_ids_ground_cluster;
-    vio->addVisualInertialStateAndOptimize(
-          timestamp_k, // current time for fixed lag smoother
-          all_measurements[k], // vision data
-          imu_stamps, imu_accgyr,
-          mesh_lmk_ids_ground_cluster); // inertial data
+    // Process data with VIO.
+    regular_vio->addVisualInertialStateAndOptimize(
+          timestamp_k, // Current time for fixed lag smoother.
+          all_measurements[k], // Vision data.
+          imu_stamps, imu_accgyr, // Inertial data.
+          mesh_lmk_ids_ground_cluster); // Lmk ids with regularities.
 
-    NonlinearFactorGraph nlfg = vio->smoother_->getFactors();
+    NonlinearFactorGraph nlfg = regular_vio->smoother_->getFactors();
     size_t nrFactorsInSmoother = 0;
-    for(auto f : nlfg){ // count the number of nonempty factors
+    for (auto f: nlfg){ // Count the number of nonempty factors.
       if(f)
         nrFactorsInSmoother++;
     }
@@ -197,31 +221,31 @@ TEST(testRegularVio, robotMovingWithConstantVelocity) {
 #ifdef USE_COMBINED_IMU_FACTOR
     EXPECT(nrFactorsInSmoother == 3 + k + 8); // 3 priors, 1 imu per time stamp, 8 smart factors
 #else
-    if(k==1){
-      EXPECT(nrFactorsInSmoother == 3 + 2*k); // 3 priors, 1 imu + 1 between per time stamp: we do not include smart factors of length 1
-    }else{
-      EXPECT(nrFactorsInSmoother == 3 + 2*k + 8); // 3 priors, 1 imu + 1 between per time stamp, 8 smart factors
+    if (k == 1) {
+      EXPECT(nrFactorsInSmoother == 3 + 2 * k); // 3 priors, 1 imu + 1 between per time stamp: we do not include smart factors of length 1
+    } else {
+      EXPECT(nrFactorsInSmoother == 3 + 2 * k + 8); // 3 priors, 1 imu + 1 between per time stamp, 8 smart factors
     }
 #endif
     // Check the results!
-    Values& results = vio->state_;
+    Values& results = regular_vio->state_;
 
-    for (int f_id = 0; f_id <= k; f_id++) {
-      Pose3 W_Pose_Blkf = results.at<Pose3>(Symbol('x', f_id));
-      Vector3 W_Vel_Blkf = results.at<Vector3>(Symbol('v', f_id));
-      ImuBias imu_bias_lkf = results.at<ImuBias>(Symbol('b', f_id));
+    for (int frame_id = 0; frame_id <= k; frame_id++) {
+      Pose3 W_Pose_Blkf     = results.at<Pose3>  (Symbol('x', frame_id));
+      Vector3 W_Vel_Blkf    = results.at<Vector3>(Symbol('v', frame_id));
+      ImuBias imu_bias_lkf  = results.at<ImuBias>(Symbol('b', frame_id));
 
-      EXPECT(assert_equal(poses[f_id].first, W_Pose_Blkf, tol));
+      EXPECT(assert_equal(poses.at(frame_id).first, W_Pose_Blkf, tol));
       EXPECT((W_Vel_Blkf - v).norm() < tol);
       EXPECT((imu_bias_lkf - imu_bias).vector().norm() < tol);
     }
   }
 }
 
-/* ************************************************************************* */
+/* ************************************************************************** */
 int main() {
   // Initialize the data!
   // initializeData();
 
   TestResult tr; return TestRegistry::runAllTests(tr); }
-/* ************************************************************************* */
+/* ************************************************************************** */
