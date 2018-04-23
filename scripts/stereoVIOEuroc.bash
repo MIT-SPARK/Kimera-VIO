105b2d2a376af14a1b7549de45aa0a3c3bd6a0d8
Toni Rosinol <tonirosinol@hotmail.com>
2018-04-23 13:09:36 -0400

    Do not visualize

--- scripts/stereoVIOEuroc.bash
+++ scripts/stereoVIOEuroc.bash
@@ -11,6 +11,7 @@ $PROJECT_PATH/build/stereoVIOEuroc \
 --vio_params_path="${PROJECT_PATH}/params/vioParameters.yaml" \
 --tracker_params_path="${PROJECT_PATH}/params/trackerParameters.yaml" \
 --backend_type=1 \
+--visualize=0 \
 --viz_type=5 \
 --log_output=false \
 --v=0 # Verbosity level.
