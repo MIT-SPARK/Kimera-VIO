1398ca78e5cd196fd8c8f7bf00e8179a56f35e6c
Toni Rosinol <tonirosinol@hotmail.com>
2018-05-28 15:42:09 -0400

    Use initial and final frame in matlab

--- matlab/RUN_STEREO_VIO.m
+++ matlab/RUN_STEREO_VIO.m
@@ -2,7 +2,7 @@ disp('running script:')
 
 appName = 'build/stereoVIOEuroc';
 
-useSudo = 0
+useSudo = 0;
 if exist('useSudo', 'var') && useSudo == 0
     sudoName = '';
 else
@@ -21,7 +21,13 @@ end
 %         ' > out-',num2str(i),'.txt');
 % end
 
- 
+if (not(exist('initialFrameID')) || initialFrameID == -1)
+    initialFrameID = 50;
+end
+if (not(exist('finalFrameID')) || finalFrameID == -1)
+    finalFrameID = 2812;
+end
+
 myCommand = horzcat('%s../%s ', ...
 '--dataset_path=', datasetPath, ...
 ' --vio_params_path=', filenameVioParams, ...
@@ -29,7 +35,10 @@ myCommand = horzcat('%s../%s ', ...
 ' --backend_type=1', ... % backend_type: 0 for Normal VIO, 1 for Regular VIO.
 ' --log_output=true', ...
 ' --visualize=true', ...
-' --viz_type=5', ' &> out-', num2str(i),'.txt');
+' --viz_type=5', ...
+' --initial_k=',  num2str(initialFrameID), ...
+' --final_k=',  num2str(finalFrameID), ...
+' &> out-', num2str(i),'.txt');
 
 myCommand = sprintf(myCommand, sudoName, appName);
 
