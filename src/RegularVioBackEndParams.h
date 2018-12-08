1082cc0f5323081705f94fca0cc6f2b2cff8fb9d
Toni Rosinol <tonirosinol@hotmail.com>
2018-12-08 17:04:24 -0500

    When using default params for RegularVio, use same as normalVIO

--- src/RegularVioBackEndParams.h
+++ src/RegularVioBackEndParams.h
@@ -35,28 +35,6 @@ class RegularVioBackEndParams: public VioBackEndParams
 {
 public:
   RegularVioBackEndParams(
-      // IMU PARAMS
-      const double gyroNoiseDensity = 0.00016968,
-      const double accNoiseDensity = 0.002,
-      const double gyroBiasSigma = 1.9393e-05,
-      const double accBiasSigma = 0.003,
-      const double imuIntegrationSigma = 1e-8,
-      const gtsam::Vector3 n_gravity = gtsam::Vector3(0.0,0.0,-9.81), // gravity in navigation frame, according to
-      const double nominalImuRate = 0.005,
-      // INITIALIZATION SETTINGS
-      const bool autoInitialize = false,
-      const bool roundOnAutoInitialize = false,
-      const double initialPositionSigma = 0.00001,
-      const double initialRollPitchSigma = 10.0 / 180.0 * M_PI,
-      const double initialYawSigma = 0.1 / 180.0 * M_PI,
-      const double initialVelocitySigma = 1e-3,
-      const double initialAccBiasSigma = 0.1,
-      const double initialGyroBiasSigma = 0.01,
-      // http://projects.asl.ethz.ch/datasets/doku.php?id=kmavvisualinertialdatasets, the x axis points upwards
-      // VISION PARAMS
-      const gtsam::LinearizationMode linMode = gtsam::HESSIAN,
-      const gtsam::DegeneracyMode degMode = gtsam::ZERO_ON_DEGENERACY,
-      const double smartNoiseSigma = 3,
       // TODO inherit from this for regularVIO backend
       const double monoNoiseSigma = 3, // for regularVioBackEnd only.
       const size_t monoNormType = 0, // for regularVioBackEnd only.
@@ -65,73 +43,15 @@ public:
       const double regularityNoiseSigma = 0.1, // for regularVioBackEnd only.
       const double huberParam = 1.345, // for regularVioBackEnd only.
       const double tukeyParam = 4.6851, // for regularVioBackEnd only.
-      const size_t regularityNormType = 2, // for regularVioBackEnd only. 0: norm 2, 1: Huber, 2: Tukey.
-      const double rankTolerance = 1, // we might also use 0.1
-      const double landmarkDistanceThreshold = 20, // max distance to triangulate point in meters
-      const double outlierRejection = 8, // max acceptable reprojection error // before tuning: 3
-      const double retriangulationThreshold = 1e-3,
-      const bool addBetweenStereoFactors = true,
-      const double betweenRotationPrecision = 0.0, // inverse of variance
-      const double betweenTranslationPrecision = 1/(0.1*0.1), // inverse of variance
-      // OPTIMIZATION PARAMS
-      const double relinearizeThreshold = 1e-2, // Before tuning: 1e-3
-      const double relinearizeSkip = 1,
-      const double zeroVelocitySigma = 1e-3, // zero velocity prior when disparity is low
-      const double noMotionPositionSigma = 1e-3,
-      const double noMotionRotationSigma = 1e-4,
-      const double constantVelSigma = 1e-2,
-      const size_t numOptimize = 2,
-      const double horizon = 6, // in seconds
-      const bool useDogLeg = false
+      const size_t regularityNormType = 2 // for regularVioBackEnd only. 0: norm 2, 1: Huber, 2: Tukey.
       ) :
     monoNoiseSigma_(monoNoiseSigma), stereoNoiseSigma_(stereoNoiseSigma),
     regularityNoiseSigma_(regularityNoiseSigma),
     monoNormType_(monoNormType), stereoNormType_(stereoNormType),
     regularityNormType_(regularityNormType),
     huberParam_(huberParam), tukeyParam_(tukeyParam),
-    VioBackEndParams(
-      gyroNoiseDensity,
-      accNoiseDensity,
-      gyroBiasSigma,
-      accBiasSigma,
-      imuIntegrationSigma,
-      n_gravity, // gravity in navigation frame, according to
-      nominalImuRate,
-      // INITIALIZATION SETTINGS
-      autoInitialize,
-      roundOnAutoInitialize,
-      initialPositionSigma,
-      initialRollPitchSigma,
-      initialYawSigma,
-      initialVelocitySigma,
-      initialAccBiasSigma,
-      initialGyroBiasSigma,
-      // http://projects.asl.ethz.ch/datasets/doku.php?id=kmavvisualinertialdatasets, the x axis points upwards
-      // VISION PARAMS
-      linMode,
-      degMode,
-      smartNoiseSigma,
-      rankTolerance, // we might also use 0.1
-      landmarkDistanceThreshold, // max distance to triangulate point in meters
-      outlierRejection, // max acceptable reprojection error // before tuning: 3
-      retriangulationThreshold,
-      addBetweenStereoFactors,
-      betweenRotationPrecision, // inverse of variance
-      betweenTranslationPrecision, // inverse of variance
-      // OPTIMIZATION PARAMS
-      relinearizeThreshold, // Before tuning: 1e-3
-      relinearizeSkip,
-      zeroVelocitySigma, // zero velocity prior when disparity is low
-      noMotionPositionSigma,
-      noMotionRotationSigma,
-      constantVelSigma,
-      numOptimize,
-      horizon, // in seconds
-      useDogLeg)
-  {
-    // Trivial sanity checks.
-    CHECK(horizon >= 0);
-  }
+    VioBackEndParams() // Use the default parameters.
+  {}
 
   double monoNoiseSigma_, stereoNoiseSigma_, regularityNoiseSigma_;
   int monoNormType_, stereoNormType_, regularityNormType_;
