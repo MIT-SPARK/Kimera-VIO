f517d0fc71b4d0ce5aaaebdb980313b2e9e34b22
Toni Rosinol <tonirosinol@hotmail.com>
2018-12-23 13:39:59 -0500

    Comment out visualization in tests

--- tests/testStereoFrame.cpp
+++ tests/testStereoFrame.cpp
@@ -238,8 +238,8 @@ TEST(testStereoFrame, findMatchingKeypointRectified) {
             matches.push_back(cv::DMatch(0, 0, 0));
             Mat canvas = UtilsOpenCV::DrawCornersMatches(left_img, left_corners,
                 right_img, right_corners, matches);
-            imshow("matched keypoints", canvas);
-            waitKey(0);
+            //imshow("matched keypoints", canvas);
+            //waitKey(0);
           }
         }
       }
@@ -573,8 +573,8 @@ TEST(testStereoFrame, getRightKeypointsRectified) {
             matches.push_back(cv::DMatch(0, 0, 0));
             Mat canvas = UtilsOpenCV::DrawCornersMatches(left_img, left_corners,
                 right_img, right_corners, matches);
-            imshow("matched keypoints", canvas);
-            waitKey(0);
+            //imshow("matched keypoints", canvas);
+            //waitKey(0);
           }
         }
       }
