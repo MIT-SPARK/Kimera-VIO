1237f37ed6858bdbec294003f26f75734d96559a
Sandro Berchier <berchier@mit.edu>
2019-06-05 20:28:18 -0400

    Added todo (Jenkins vs PC)

--- tests/testStereoFrame.cpp
+++ tests/testStereoFrame.cpp
@@ -940,7 +940,7 @@ TEST(testStereoFrame, undistortFisheye) {
 }
 
 /* *************************************************************************
-// TODO: Figure out why this compiles on PC, but not on Jenkins
+// TODO: Figure out issue PC vs. Jenkins
 TEST(testStereoFrame, undistortFisheyeStereoFrame) {
 
   // Parse camera params for left and right cameras
@@ -1020,11 +1020,9 @@ TEST(testStereoFrame, undistortFisheyeStereoFrame) {
                                                   sf->getBaseline());
   
   // Test distortion with image comparison --> uncomment
-  EXPECT(UtilsOpenCV::CvMatCmp(undist_sidebyside, undist_sidebyside_ref, 1e-1));
-
+  EXPECT(UtilsOpenCV::CvMatCmp(undist_sidebyside, undist_sidebyside_ref, 1e-3));
 
 }
-
 */
 
 /* ************************************************************************* */
