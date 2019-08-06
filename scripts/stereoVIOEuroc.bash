7641df1254a21078c476bbbd7a7021dee5043ef0
yunchang <yunchang@mit.edu>
2019-08-06 13:20:20 -0400

    update kitti readme

--- scripts/stereoVIOEuroc.bash
+++ scripts/stereoVIOEuroc.bash
@@ -86,7 +86,7 @@ echo """ Launching:
   --log_prefix=0 \
   --dataset_path="$DATASET_PATH" \
   --vio_params_path="$VIO_PARAMS_PATH" \
-  --initial_k=150 \
+  --initial_k=50 \
   --final_k=2000 \
   --tracker_params_path="$TRACKER_PARAMS_PATH" \
   --flagfile="../params/flags/stereoVIOEuroc.flags" \
