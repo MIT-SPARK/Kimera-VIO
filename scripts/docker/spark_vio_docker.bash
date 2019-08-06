6d402651e80581822bde145398937d03db2fc1b6
Toni <tonirosinol@hotmail.com>
2019-08-06 13:54:28 -0400

    Add script to run and display sparkVIO in docker

--- scripts/docker/spark_vio_docker.bash
+++ scripts/docker/spark_vio_docker.bash
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Allow X server connection
+xhost +local:root
 docker run -it --rm \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    spark_vio \
-    bash -ic "./VIO/scripts/spark_vio.bash -p ./data/V1_01_easy -r -parallel"
+    spark_vio
+# Disallow X server connection
+xhost -local:root
