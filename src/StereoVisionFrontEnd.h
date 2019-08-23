9329e92462ae21a610ff72bbb6fef0db0773503b
Toni <tonirosinol@hotmail.com>
2019-08-22 22:35:48 -0400

    Use pragma once, unsafe header guards

--- src/StereoVisionFrontEnd.h
+++ src/StereoVisionFrontEnd.h
@@ -12,8 +12,7 @@
  * @author Antoni Rosinol, Luca Carlone
  */
 
-#ifndef StereoVisionFrontEnd_H_
-#define StereoVisionFrontEnd_H_
+#pragma once
 
 #include <boost/shared_ptr.hpp> // used for opengv
 
@@ -33,7 +32,6 @@
 #include "Tracker-definitions.h"
 #include "StereoVisionFrontEnd-definitions.h"
 #include "VioBackEnd-definitions.h"
-
 #include "logging/Logger.h"
 
 namespace VIO {
@@ -292,5 +290,4 @@ private:
 };
 
 } // namespace VIO
-#endif /* StereoVisionFrontEnd_H_ */
 
