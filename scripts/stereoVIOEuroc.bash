c88f65a6184991603a46b95f83e1658d5a609062
Toni Rosinol <tonirosinol@hotmail.com>
2019-08-24 01:07:26 -0400

    Accidentally pushed changes to script

--- scripts/stereoVIOEuroc.bash
+++ scripts/stereoVIOEuroc.bash
@@ -99,5 +99,4 @@ echo """ Launching:
   --vmodule=VioBackEnd=0,RegularVioBackEnd=0,Mesher=0,StereoVisionFrontEnd=0 \
   --backend_type="$BACKEND_TYPE" \
   --parallel_run="$PARALLEL_RUN" \
-  --dataset_type="$DATASET_TYPE" \
-  --log_output=1 --output_path="../output"
+  --dataset_type="$DATASET_TYPE"
