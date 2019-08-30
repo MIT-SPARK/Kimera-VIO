7769bdd9a5c6200cd300d6b84eed5b80714cca12
Toni <tonirosinol@hotmail.com>
2019-08-30 18:36:43 -0400

    Improved yamelizer

--- scripts/euroc/yamelize.bash
+++ scripts/euroc/yamelize.bash
@@ -6,15 +6,39 @@
 # │   ├── V1_01_easy
 # │   ├── V1_02_medium
 # │   └── etc...
+
+# Either modify this variable yourself or use this script as
+# ```
+# bash yamelize.bash -p PATH_TO_YOUR_EUROC_DATASET
 EUROC_DATASET="/home/tonirv/Euroc"
+
+# Parse Options.
+if [ $# -eq 0 ]; then
+  # If there is no options tell user what are the values we are using.
+  echo "Yamelizing dataset at path: $EUROC_DATASET_PATH"
+else
+  # Parse all the options.
+  while [ -n "$1" ]; do # while loop starts
+      case "$1" in
+        # Option -p, provides path to dataset.
+      -p) EUROC_DATASET=$2
+          echo "Using dataset at path: $EUROC_DATASET"
+          shift ;;
+      --)
+          shift # The double dash which separates options from parameters
+          break
+          ;; # Exit the loop using break command
+      *) echo "Option $1 not recognized" ;;
+      esac
+      shift
+  done
+fi
+
 DIRECTORIES="$EUROC_DATASET/*"
-for i in $(ls -d $DIRECTORIES); do
-  echo Yamelizing dataset: $i
-  sed -i'' -e '1 i\\%YAML\:1\.0' $i/mav0/pointcloud0/sensor.yaml
-  sed -i'' -e '1 i\\%YAML\:1\.0' $i/mav0/vicon0/sensor.yaml
-  sed -i'' -e '1 i\\%YAML\:1\.0' $i/mav0/body.yaml
-  sed -i'' -e '1 i\\%YAML\:1\.0' $i/mav0/state_groundtruth_estimate0/sensor.yaml
-  sed -i'' -e '1 i\\%YAML\:1\.0' $i/mav0/cam1/sensor.yaml
-  sed -i'' -e '1 i\\%YAML\:1\.0' $i/mav0/cam0/sensor.yaml
-  sed -i'' -e '1 i\\%YAML\:1\.0' $i/mav0/imu0/sensor.yaml
+for euroc_dataset in $(ls -d $DIRECTORIES); do
+  echo Yamelizing dataset: $euroc_dataset
+  for file in $(find $euroc_dataset -iname '*.yaml'); do
+    echo Yamelizing file: $file
+    sed -i'' -e '1 i\\%YAML\:1\.0' $i/$file
+  done
 done
