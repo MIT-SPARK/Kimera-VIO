6cd2e6ede2aff2fbaf76e26c7fa8f4b51d006808
Toni <tonirosinol@hotmail.com>
2019-08-30 18:58:52 -0400

    Nicefy yamelize.bash

--- scripts/euroc/yamelize.bash
+++ scripts/euroc/yamelize.bash
@@ -10,12 +10,12 @@
 # Either modify this variable yourself or use this script as
 # ```
 # bash yamelize.bash -p PATH_TO_YOUR_EUROC_DATASET
-EUROC_DATASET="/home/tonirv/Euroc"
-
 # Parse Options.
 if [ $# -eq 0 ]; then
   # If there is no options tell user what are the values we are using.
-  echo "Yamelizing dataset at path: $EUROC_DATASET_PATH"
+  echo "Please provide a Dataset path using the -p flag:
+        Usage: yamelize.bash -p PATH_TO_EUROC_DATASET"
+  exit
 else
   # Parse all the options.
   while [ -n "$1" ]; do # while loop starts
@@ -24,21 +24,36 @@ else
       -p) EUROC_DATASET=$2
           echo "Using dataset at path: $EUROC_DATASET"
           shift ;;
-      --)
-          shift # The double dash which separates options from parameters
+      --) shift # The double dash which separates options from parameters
           break
           ;; # Exit the loop using break command
-      *) echo "Option $1 not recognized" ;;
+      *) echo -e "\e[31mOption $1 not recognized"
+         exit
+         ;;
       esac
       shift
   done
 fi
 
-DIRECTORIES="$EUROC_DATASET/*"
-for euroc_dataset in $(ls -d $DIRECTORIES); do
-  echo Yamelizing dataset: $euroc_dataset
-  for file in $(find $euroc_dataset -iname '*.yaml'); do
-    echo Yamelizing file: $file
+# Check EUROC_DATASET is not empty:
+if [ -z "$EUROC_DATASET" ]; then
+  echo -e "\e[31mDataset path cannot be empty: use -p flag"
+  exit
+fi
+
+LIST_INODES=$(ls -d $EUROC_DATASET/*)
+if [ ${#LIST_INODES[@]} -eq 0 ]; then
+  echo -e "\e[31mNo datasets found..."
+  exit
+fi
+for euroc_dataset in $LIST_INODES; do
+  echo -e "\e[32mYamelizing dataset: $euroc_dataset\033[0m"
+  YAML_FILES=$(find $euroc_dataset -iname '*.yaml')
+  if [ ${#YAML_FILES[@]} -eq 0 ]; then
+    echo -e "\e[31mNo yaml files found..."
+  fi
+  for file in $YAML_FILES; do
+    echo -e "Yamelizing file: $file"
     sed -i'' -e '1 i\\%YAML\:1\.0' $i/$file
   done
 done
