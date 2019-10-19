4534d346212c2478f036c99f2c16ac76134f75f9
Toni <tonirosinol@hotmail.com>
2019-10-19 19:36:57 -0400

    Add license header to factors

--- include/kimera-vio/factors/ParallelPlaneRegularFactor.h
+++ include/kimera-vio/factors/ParallelPlaneRegularFactor.h
@@ -1,3 +1,11 @@
+/* ----------------------------------------------------------------------------
+ * Copyright 2017, Massachusetts Institute of Technology,
+ * Cambridge, MA 02139
+ * All Rights Reserved
+ * Authors: Luca Carlone, et al. (see THANKS for the full author list)
+ * See LICENSE for the license information
+ * -------------------------------------------------------------------------- */
+
 /*
  * @file ParallelPlaneRegularFactor.h
  * @brief ParallelPlaneRegular Factor class
