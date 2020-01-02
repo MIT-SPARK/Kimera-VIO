7890d83bde04c729b8466ec73c17d6074381b90f
Toni <tonirosinol@hotmail.com>
2020-01-02 15:13:10 -0500

    Remove feature selector from tracker

--- include/kimera-vio/frontend/Tracker.h
+++ include/kimera-vio/frontend/Tracker.h
@@ -24,16 +24,13 @@
 
 #include <gtsam/geometry/StereoCamera.h>
 
-#include "kimera-vio/frontend/FeatureSelector.h"
+#include "kimera-vio/frontend/CameraParams.h"
 #include "kimera-vio/frontend/Frame.h"
 #include "kimera-vio/frontend/OpticalFlowPredictor.h"
 #include "kimera-vio/frontend/StereoFrame.h"
 #include "kimera-vio/frontend/Tracker-definitions.h"
 #include "kimera-vio/utils/Macros.h"
 #include "kimera-vio/utils/UtilsOpenCV.h"
-
-// implementation of feature selector, still within the tracker class
-#include <gtsam/nonlinear/Marginals.h>
 
 namespace VIO {
 
