d2e446206649051cf6e2676a6c400a861faa8479
Toni <tonirosinol@hotmail.com>
2020-02-11 19:08:29 -0500

    Add command line parsing for tests

--- tests/testKimeraVIO.cpp
+++ tests/testKimeraVIO.cpp
@@ -5,7 +5,10 @@
 DEFINE_string(test_data_path, "../tests/data", "Path to data for unit tests.");
 
 int main(int argc, char **argv) {
+  // Initialize Google's testing library.
   ::testing::InitGoogleTest(&argc, argv);
+  // Initialize Google's flags library.
+  google::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
   return RUN_ALL_TESTS();
 }
