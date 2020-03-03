dcd62ec4a709eb18f336c9b1edffb63d47379bca
Andrew Violette <aviolett@mit.edu>
2020-03-02 19:14:13 -0500

    Fleshed out the dummy variables in testDataProviderModule, loop fix

--- tests/testDataProviderModule.cpp
+++ tests/testDataProviderModule.cpp
@@ -37,26 +37,36 @@ class TestDataProviderModule : public ::testing::Test {
   TestDataProviderModule() {  }
 
  protected:
+  VIO::StereoVisionFrontEndModule::InputQueue* dummy_queue;
   VIO::DataProviderModule::UniquePtr data_provider_module_;
 
   void SetUp() override {
-    // Clear the output queue
-    while (!output_queue.empty()) {
-      output_queue.pop();
-    }
+    // Set google flags to assume image is already rectified--makes dummy params
+    // easier
+    int fake_argc = 2;
+    char** fake_argv =
+        reinterpret_cast<char**>(malloc(sizeof(char*) * fake_argc));
+    fake_argv[0] = "foo";
+    fake_argv[1] = "--images_rectified";
+    google::ParseCommandLineFlags(&fake_argc, &fake_argv, true);
 
     // Create DataProvider
-    VIO::StereoVisionFrontEndModule::InputQueue dummy_queue("unused");
+    dummy_queue = new VIO::StereoVisionFrontEndModule::InputQueue("unused");
     VIO::StereoMatchingParams dummy_params;
     bool parallel = false;
     data_provider_module_ = VIO::make_unique<VIO::DataProviderModule>(
-        &dummy_queue, "Data Provider", parallel, dummy_params);
+        dummy_queue, "Data Provider", parallel, dummy_params);
     data_provider_module_->registerVioPipelineCallback(
         std::bind(SaveOutput, std::placeholders::_1));
   }
 
   void TearDown() override {
     data_provider_module_->shutdown();
+    delete dummy_queue;
+    // Clear the output queue
+    while (!output_queue.empty()) {
+      output_queue.pop();
+    }
   }
 
   static void SaveOutput(VIO::StereoImuSyncPacket::UniquePtr sync_packet) {
@@ -66,40 +76,53 @@ class TestDataProviderModule : public ::testing::Test {
   VIO::Frame::UniquePtr MakeDummyFrame(const VIO::FrameId& frame_id,
                                        const VIO::Timestamp& timestamp) {
     return VIO::make_unique<VIO::Frame>(
-        frame_id, timestamp, VIO::CameraParams(), cv::Mat());
+        frame_id, timestamp, MakeDumyCameraParams(), cv::Mat());
+  }
+
+  VIO::CameraParams MakeDumyCameraParams() {
+    VIO::CameraParams dummy_params;
+    dummy_params.R_rectify_ = cv::Mat::zeros(3, 3, CV_32F);
+    dummy_params.P_ = cv::Mat::zeros(3, 3, CV_32F);
+    return dummy_params;
   }
 };
 
 /* ************************************************************************* */
 TEST_F(TestDataProviderModule, basicSequentialCase) {
   VIO::FrameId current_id = 0;
-  VIO::Timestamp current_time = 10;
+  VIO::Timestamp current_time = 10;  // 0 has special meaning, offset by 10
+
+  // Fencepost frames with IMU data
+  data_provider_module_->fillImuQueue(
+      VIO::ImuMeasurement(++current_time, VIO::ImuAccGyr::Zero()));
+
   data_provider_module_->fillLeftFrameQueue(
-      MakeDummyFrame(current_id++, ++current_time));
+      MakeDummyFrame(++current_id, ++current_time));
   data_provider_module_->fillRightFrameQueue(
-      MakeDummyFrame(current_id++, current_time));
+      MakeDummyFrame(current_id, current_time));
   data_provider_module_->spin();
 
   size_t num_imu_to_make = 4;
   for (size_t i = 0; i < num_imu_to_make; i++) {
     data_provider_module_->fillImuQueue(
-        VIO::ImuMeasurement(++current_time, VIO::ImuAccGyr()));
+        VIO::ImuMeasurement(++current_time, VIO::ImuAccGyr::Zero()));
   }
 
   data_provider_module_->fillLeftFrameQueue(
-      MakeDummyFrame(current_id++, ++current_time));
+      MakeDummyFrame(++current_id, ++current_time));
   data_provider_module_->fillRightFrameQueue(
-      MakeDummyFrame(current_id++, current_time));
+      MakeDummyFrame(current_id, current_time));
+  // IMU and camera streams are not necessarily in sync
+  // Send an IMU packet after camera packets to signal no more IMU incoming
+  data_provider_module_->fillImuQueue(
+      VIO::ImuMeasurement(++current_time, VIO::ImuAccGyr::Zero()));
   data_provider_module_->spin();
-
-  // Leave some time for data_provider_module_ to finish its work.
-  // std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
   VIO::StereoImuSyncPacket::UniquePtr result;
   CHECK(output_queue.popBlocking(result));
   CHECK(result);
-  EXPECT_EQ(current_time, result->timestamp_);
-  EXPECT_EQ(static_cast<VIO::FrameId>(3),
+  EXPECT_EQ(current_time - 1, result->timestamp_);
+  EXPECT_EQ(static_cast<VIO::FrameId>(2),
             result->getStereoFrame().getFrameId());
   EXPECT_EQ(num_imu_to_make, result->getImuStamps().size());
 }
