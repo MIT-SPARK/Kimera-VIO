d4eed0410e71bf1e13696e576abe3326c9885058
Toni <tonirosinol@hotmail.com>
2020-03-19 18:11:16 -0400

    Color test output

--- tests/testKimeraVIO.cpp
+++ tests/testKimeraVIO.cpp
@@ -6,7 +6,9 @@ DEFINE_string(test_data_path, "../tests/data", "Path to data for unit tests.");
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
+  google::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
   FLAGS_logtostderr = 1;
+  FLAGS_colorlogtostderr = 1;
   return RUN_ALL_TESTS();
 }
