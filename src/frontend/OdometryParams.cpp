e535730d5867f8a3bddeff6363711ff15920d096
mabate <mabate@mit.edu>
2021-04-19 15:10:49 -0400

    Add OdometryParams

---
+++ src/frontend/OdometryParams.cpp
@@ -0,0 +1,58 @@
+/* ----------------------------------------------------------------------------
+ * Copyright 2017, Massachusetts Institute of Technology,
+ * Cambridge, MA 02139
+ * All Rights Reserved
+ * Authors: Luca Carlone, et al. (see THANKS for the full author list)
+ * See LICENSE for the license information
+ * -------------------------------------------------------------------------- */
+
+/**
+ * @file   OdometryParams.cpp
+ * @brief  Params for Odometry Sensor.
+ * @author Marcus Abate
+ */
+
+#include "kimera-vio/frontend/OdometryParams.h"
+
+#include <glog/logging.h>
+
+#include "kimera-vio/utils/UtilsOpenCV.h"
+#include "kimera-vio/utils/YamlParser.h"
+
+namespace VIO {
+
+OdometryParams::OdometryParams() : PipelineParams("Odometry params") {}
+
+bool OdometryParams::parseYAML(const std::string& filepath) {
+  YamlParser yaml_parser(filepath);
+
+  std::vector<double> vector_pose;
+  yaml_parser.getNestedYamlParam("T_BS", "data", &vector_pose);
+  const gtsam::Pose3& body_Pose_cam =
+      UtilsOpenCV::poseVectorToGtsamPose3(vector_pose);
+
+
+  double rate_hz = 0.0;
+  yaml_parser.getYamlParam("rate_hz", &rate_hz);
+  CHECK_GT(rate_hz, 0.0);
+  nominal_sampling_time_s_ = 1.0 / rate_hz;
+
+  return true;
+}
+
+void OdometryParams::print() const {
+  std::stringstream out;
+  PipelineParams::print(out,
+                        "body_Pose_odom",
+                        body_Pose_odom_,
+                        "nominal_sampling_time_s",
+                        nominal_sampling_time_s_);
+  LOG(INFO) << out.str();
+}
+
+bool OdometryParams::equals(const PipelineParams& obj) const {
+  const auto& rhs = static_cast<const OdometryParams&>(obj);
+  return body_Pose_odom_.equals(rhs.body_Pose_odom_);
+}
+
+}  // namespace VIO
