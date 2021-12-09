1c7cfc517e9610e049e10a70882c44ffaaabf37c
Dominic <dmaggio@draper.com>
2021-12-09 12:45:10 -0500

    increasing verbosity to check jenkins

--- src/pipeline/Pipeline.cpp
+++ src/pipeline/Pipeline.cpp
@@ -180,7 +180,7 @@ bool Pipeline::shutdownWhenFinished(const int& sleep_time_ms,
   }
   LOG(INFO) << "Shutting down VIO, reason: input is empty and threads are "
                "idle.";
-  VLOG(5) << printStatus();
+  VLOG(1) << printStatus();
   if (!shutdown_) shutdown();
   return true;
 }
